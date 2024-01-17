32b737543742192ffcf83f295bd310959e96bc81
irsyadagungh <irsyadagung08@gmail.com>
2024-01-17 23:11:56 +0700

    fix typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div className="container  flex gap-10 md:gap-0 px-10 md:px-0 flex-wrap justify-evenly">
                 {/* First Div */}
                 <div className="felx w-full md:w-1/3 flex-col">
-                    <h1 className="text-bone mb-4 text-2xl">Business Inquires 📨</h1>
+                    <h1 className="text-bone mb-4 text-2xl">Business Inquiries 📨</h1>
                     <span className="flex gap-4 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                             <path className="fill-current text-bone" d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/>
